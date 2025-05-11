5cb48b8e446f7161336a2e5c689057c990bf2139
AKASH POLKAR <akashpolkar1234@gmail.com>
2025-05-12 01:16:12 +0200

    File Added

---
+++ commands.py
@@ -0,0 +1,13 @@
+COMMAND_MAP = {
+    "start": ["start", "activate", "begin operation", "power up"],
+    "stop": ["stop", "deactivate", "shutdown", "halt"],
+    "move up": ["move up", "go higher", "lift arm", "raise"],
+    "move down": ["move down", "lower arm", "go lower", "drop"],
+    "rotate": ["rotate", "spin", "turn around", "rotate arm"],
+    "grip": ["grip", "grab", "hold", "pick"],
+    "release": ["release", "drop object", "let go"],
+    "move left": ["move left", "turn left", "shift left"],
+    "move right": ["move right", "turn right", "shift right"],
+    "pick the object": ["pick the object", "grab item", "pick up", "pick it"],
+    "stop immediately": ["stop immediately", "emergency stop", "halt now"]
+}
